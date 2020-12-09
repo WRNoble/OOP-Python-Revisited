889b598a359e71ab5e9849fc3eb4dc2737d4d810
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-12-09 08:16:11 -0500

    modifying properties in class

--- OOP.py
+++ OOP.py
@@ -1,7 +1,16 @@
 class Car:
-    pass
+    wheels = 4
+    doors = 4
+    engine = True
 
-my_car = Car()
+car_one = Car()
+car_two = Car()
+print(car_one.doors)
+print(car_two.doors)
+print(Car.doors)
 
-print(my_car)
-print(type(my_car))
+Car.doors = 2
+car_one.doors = 7
+print(car_one.doors)
+print(car_two.doors)
+print(Car.doors)
